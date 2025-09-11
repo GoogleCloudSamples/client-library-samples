fb530b638e4796483c205c3deb2288b1f08dc30d
David Cavazos <dcavazos@google.com>
2025-09-11 12:22:06 -0700

    add node lint workflow

---
+++ .github/custard/map.sh
@@ -0,0 +1,20 @@
+set -e # Exit on error
+set -u # Error when expanding unset variables
+
+SCRIPT="$1"
+JSON_LIST="$2"
+
+FAILED=""
+for path in $(echo "$JSON_LIST" | jq -r '.[]'); do
+  echo "----- $SCRIPT $path -----"
+  bash "$SCRIPT" "$path" || FAILED="$FAILED\n - $path"
+  echo ""
+done
+
+echo "===================="
+if [[ -n "$FAILED" ]]; then
+  echo -e "❌ Failed: $FAILED"
+  exit 1
+else
+  echo "✅ Succeeded"
+fi
