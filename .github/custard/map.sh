2b755049859a1ddc9b5d60ac2ed6dbef9f82fca7
David Cavazos <dcavazos@google.com>
2025-09-11 13:24:33 -0700

    keep track of succeeded too

--- .github/custard/map.sh
+++ .github/custard/map.sh
@@ -4,17 +4,24 @@ set -u # Error when expanding unset variables
 SCRIPT="$1"
 JSON_LIST="$2"
 
+SUCCEEDED=""
 FAILED=""
 for path in $(echo "$JSON_LIST" | jq -r '.[]'); do
   echo "----- $path -----"
-  bash "$SCRIPT" "$path" || FAILED="$FAILED\n - $path"
+  if bash "$SCRIPT" "$path"; then
+    SUCCEEDED="$SUCCEEDED\n - $path"
+  else
+    FAILED="$FAILED\n - $path"
+  fi
   echo ""
 done
 
 echo "===================="
+if [[ -n "$SUCCEEDED" ]]; then
+  echo -e "✅ Succeeded: $SUCCEEDED\n"
+fi
+
 if [[ -n "$FAILED" ]]; then
-  echo -e "❌ Failed: $FAILED"
+  echo -e "❌ Failed: $FAILED\n"
   exit 1
-else
-  echo "✅ Succeeded"
 fi
